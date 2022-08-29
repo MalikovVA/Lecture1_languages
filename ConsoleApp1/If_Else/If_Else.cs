84c939c6444e67d70cf894735d72a8630bc550e4
Malikov Vladimir <v.a.jack@rambler.ru>
2022-08-29 11:12:17 +0300

    If_Else

---
+++ ConsoleApp1/If_Else/If_Else.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя, ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "vladimir")
+{
+    Console.WriteLine("Wow, it's a Vlaimir");
+        }    
+else
+{
+    Console.WriteLine("Hello, ");
+    Console.WriteLine(username);    
+}
